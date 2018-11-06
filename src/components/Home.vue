9ad3a2d44f28d54dfefaea8d76794faafecc5b01
david jassiel <djassiell@gmail.com>
2018-11-06 10:26:59 -0400

    first commit

---
+++ src/components/Home.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-jumbotron color="grey lighten-2">
+    <v-container fill-height>
+      <v-flex align-center>
+        <h3 class="display-3">
+          {{ $t('welcome.title', {app: 'Vuejs 2 + firestore'}) }}
+        </h3>
+
+        <span class="subheading">
+          {{ $t('welcome.content') }}
+        </span>
+
+        <v-divider class="my-3"/>
+        <div class="title mb-3">{{ $t('welcome.call_to_action') }}</div>
+
+        <v-btn large color="primary" class="mx-0" to="login" >
+          {{ $t('welcome.action_button')}}
+        </v-btn>
+      </v-flex>
+    </v-container>
+  </v-jumbotron>
+</template>
+
+<script>
+export default {
+  name: 'Home'
+}
+</script>
---
+++ src/config/firebase.js
@@ -0,0 +1,8 @@
+export default {
+    apiKey: "AIzaSyCm7nOsn6Nc5BvgK_ZVbVX7IGP5TOC_rkE",
+    authDomain: "vue-firestore-61a4b.firebaseapp.com",
+    databaseURL: "https://vue-firestore-61a4b.firebaseio.com",
+    projectId: "vue-firestore-61a4b",
+    storageBucket: "vue-firestore-61a4b.appspot.com",
+    messagingSenderId: "601177967720"
+  }
